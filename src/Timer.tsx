be2694938291346edd42a07a7155f964f9c03aac
Natasha Lawson <natasha.lawson93@gmail.com>
2020-05-12 11:19:25 -0600

    Add svg icons and deploy

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import useTimer from "./hooks/useTimer";
+import { ReactComponent as PlayIcon } from "./img/play.svg";
+import { ReactComponent as PauseIcon } from "./img/pause.svg";
+import { ReactComponent as ResetIcon } from "./img/reset.svg";
 import styles from "./Timer.module.css";
 
-export default function Timer({ defaultTime }) {
+type TimerProps = {
+  defaultTime: number;
+};
+
+export default function Timer({ defaultTime }: TimerProps) {
   const [{ formattedTime, isActive }, timerActions] = useTimer(defaultTime);
   return (
     <>
@@ -14,10 +21,10 @@ export default function Timer({ defaultTime }) {
           className={styles.button}
           onClick={(e) => timerActions.togglePause()}
         >
-          {isActive ? "II" : "▶️"}
+          {isActive ? <PauseIcon /> : <PlayIcon />}
         </button>
         <button className={styles.button} onClick={() => timerActions.reset()}>
-          Reset
+          <ResetIcon />
         </button>
       </div>
     </>
